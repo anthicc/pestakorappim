f34adbe7970ae6f856df25499876491fe896d662
Abda Zuljiva <65759448+anthicc@users.noreply.github.com>
2023-11-20 22:23:42 +0700

    Update voting.php

--- voting.php
+++ voting.php
@@ -519,11 +519,7 @@ document.addEventListener("alpine:init", () => {
  });
  </script>
                                     </div>
-                                </div>
-                            </div>
-                    </div>
-                    <!-- end main content section -->
-                </div>
+                                
 
                 <?php include ('footer.php'); ?>
     </body>
