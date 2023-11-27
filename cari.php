628488469d08a7b902494b31a99caea1a6b7c6aa
Abda Zuljiva <65759448+anthicc@users.noreply.github.com>
2023-11-28 01:42:45 +0700

    Update cari.php

--- cari.php
+++ cari.php
@@ -11,17 +11,17 @@ if ($conn->connect_error) {
 }
 
 // Query untuk mengambil data NIT dari tabel users yang tidak ada di tabel pemilihan
-$query = "SELECT users.nit FROM users LEFT JOIN pemilihan ON users.nit = pemilihan.nit WHERE pemilihan.nit IS NULL";
+$query = "SELECT nit, COUNT(*) as jumlah_duplikasi FROM pemilihan GROUP BY nit HAVING COUNT(*) > 1";
 
 $result = $conn->query($query);
 
 if ($result->num_rows > 0) {
-    echo "NIT yang tidak ada di tabel pemilihan: ";
+    echo "NIT yang memiliki duplikasi di tabel pemilihan: ";
     while ($row = $result->fetch_assoc()) {
-        echo $row['nit'] . " ";
+        echo $row['nit'] . " (Duplikasi: " . $row['jumlah_duplikasi'] . " kali) ";
     }
 } else {
-    echo "Semua NIT pada tabel users sudah ada di tabel pemilihan.";
+    echo "Tidak ada NIT yang memiliki duplikasi di tabel pemilihan.";
 }
 
 // Tutup koneksi
